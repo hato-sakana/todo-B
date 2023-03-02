6ba49eb18ab6a739d92687868a12a3f00f48e423
hato-sakana <taka070762@gmail.com>
2023-03-02 15:41:14 +0800

    postsの作成、web.php

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="#">
+</head>
+<body>
+    <header>
+        <div class="header-left">
+            <img class="logo" src="#" alt="">
+        </div>
+        <div class="header-right">
+            <ul class="nav">
+                <li><a href="#">記事作成</a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <section class="welcome">
+            <h1>投稿一覧</h1>
+                <div>  {{-- 実際のデータ --}}
+                    <h2>タイトル</h2>    {{-- 実際のタイトル --}}
+                    <p>内容</p>   {{-- 実際の内容 --}} 
+                </div>
+        </section>
+    </main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('posts', 'PostController'); 
+
+
+Route::resource('comments', 'CommentController'); 
