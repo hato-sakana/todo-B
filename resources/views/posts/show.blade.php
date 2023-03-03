410637f51bf95210f628ba7483f58473ef8734a8
reiokirenai <124994924+reiokirenai@users.noreply.github.com>
2023-03-03 14:03:50 +0800

    詳細表示 show.blade.phpの中身

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>詳細画面</title>
+</head>
+<body>
+    <header>
+        <div class="header-left">
+            <img class="logo" src="#" alt="">
+        </div>
+        <div class="header-right">
+            <ul class="nav">
+                <li><a href="#">ユーザーA</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class = "container" >
+        <div class="detail-title">
+            <h3>タイトル：</h3>
+        </div>
+        <div class="detail-body">
+            <p class="text">内容：</p>
+        </div>
+    </div>
+        <a href="#">編集する</a>
+        <form action="#" method="post">
+            <input type="submit" value="削除" class="btn btn-delete" onclick="return confirm("本当に消すの？");">
+        </form>
+    
+    
+</body>
+</html>
