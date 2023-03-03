fc5dde021f254191f3a7432bae5cc59b93eb2e71
AkaneFutai <aooooo05314159@gmail.com>
2023-03-03 13:41:24 +0800

    createメソッド追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,12 @@ class PostController extends Controller
 
         return view('posts.show', ['post'=>$post]);
     }
+
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    
 }
