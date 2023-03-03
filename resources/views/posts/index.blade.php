ee0dc123fbbb0ad02c69ee6684824e06744f628d
hato-sakana <taka070762@gmail.com>
2023-03-03 13:34:35 +0800

    詳細ボタン追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,34 +1,54 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
-  <link rel="stylesheet" href="#">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <header>
-        <div class="header-left">
-            <img class="logo" src="#" alt="">
+  <header>
+    <div class="header-left">
+            <img class="logo" src="./logo.png" alt="">
         </div>
         <div class="header-right">
             <ul class="nav">
-                <li><a href="#">記事作成</a></li>
+                <li><a href="#">ユーザA</a></li>
             </ul>
         </div>
-    </header>
-    <main>
-        <section class="welcome">
-            <h1>投稿一覧</h1>
-                <div>  {{-- 実際のデータ --}}
-                    <h2>タイトル</h2>    {{-- 実際のタイトル --}}
-                    <p>内容</p>   {{-- 実際の内容 --}} 
-                    <ul class="nav1">
-                        <li><a href="#">詳細</a></li>
-                    </ul>
-                </div>
-        </section>
-    </main>
+  </header>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+             <div class="card text-center">
+            <div class="card-header">
+                投稿一覧
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">タイトル : おはよう</h5>
+                <p class="card-text">
+                  内容 : 今日のセブは快晴
+                </p>
+                <p class="card-text">投稿者：Seed Techさん</p>
+                <a href="#" class="btn btn-primary">詳細へ</a>
+            </div>
+            <div class="card-footer text-muted">
+                投稿日時 : 2021/11/08
+            </div>
+        </div>
+        </div>
+        <div class="col-md-2">
+          <a href="#" class="btn btn-primary">
+            新規投稿
+          </a>
+        </div>
+    </div>
+  </div>
+  <footer>
+    Copyright &copy; Seedkun Inc.
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 </body>
 </html>
