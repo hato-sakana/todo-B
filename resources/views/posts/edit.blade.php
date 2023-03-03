532560896e3f208de490635804c36f1e3a9dc55f
reiokirenai <124994924+reiokirenai@users.noreply.github.com>
2023-03-03 16:24:07 +0800

    edit.blade.php簡易

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>編集画面</title>
+</head>
+<body>
+    <header>
+        <div class="header-left">
+            <img class="logo" src="#" alt="">
+        </div>
+        <div class="header-right">
+            <ul class="nav">
+                <li><a href="#">ユーザーA</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class = "container-edit" >
+        <div class="detail-title">
+            <h3>タイトル：</h3>
+        </div>
+        <div class="detail-body">
+            <p class="text">内容：</p>
+        </div>
+    </div>
+        <a href="#">更新する</a>
+       
+    
+    
+</body>
+</html>
